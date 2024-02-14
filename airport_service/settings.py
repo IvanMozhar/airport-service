e2aaec939b777bf5ce21f8d6a0dbf4ff87e70465
Ivan <mozharivan17@gmail.com>
2024-02-14 07:44:49 +0200

    Add user, airport and rest_framework apps to settings

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "airport"
+    "user"
 ]
 
 MIDDLEWARE = [
@@ -99,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
