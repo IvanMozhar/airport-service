e8050071f17a07b82475de8244532f9993b93f3f
Ivan <mozharivan17@gmail.com>
2024-02-16 08:18:23 +0200

    Add user filter to order

--- airport/views.py
+++ airport/views.py
@@ -105,6 +105,9 @@ class OrderViewSet(
     serializer_class = OrderSerializer
     permission_classes = (IsAuthenticated,)
 
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
     def get_serializer_class(self):
         if self.action == "list":
             return OrderListSerializer
