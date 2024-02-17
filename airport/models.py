6677cb4d1d2414c68596e1e2ce9e639ae54297f2
Ivan <mozharivan17@gmail.com>
2024-02-17 04:47:12 +0200

    Fix airplane image file path field

--- airport/models.py
+++ airport/models.py
@@ -58,7 +58,7 @@ def airplane_image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("uploads/movies/", filename)
+    return os.path.join("uploads/airplanes/", filename)
 
 
 class Airplane(models.Model):
@@ -71,7 +71,7 @@ class Airplane(models.Model):
         null=True,
         related_name="airplanes"
     )
-    image = models.ImageField(null=True, upload_to=movie_image_file_path)
+    image = models.ImageField(null=True, upload_to=airplane_image_file_path)
 
     @property
     def capacity(self) -> int:
